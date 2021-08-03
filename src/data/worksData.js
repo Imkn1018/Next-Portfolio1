d0938d271f4ab0d0cb9b40a6ad18b3068cc583c8
Imkn1018 <o2oneru2505@gmail.com>
2021-08-03 11:43:37 +0900

    ポートフォリオデータの追加

---
+++ src/data/worksData.js
@@ -0,0 +1,24 @@
+export default [
+  {
+    image: 'images/toppage.png',
+    title: 'My Top Page',
+    path: '',
+    description: '自己紹介用のHPとなります。',
+    createdDate: 'July,2021',
+  },
+  {
+    image: 'images/wisher.png',
+    title: 'wisher',
+    path: '',
+    description: '人生で叶えたいことを記録しておくためのアプリです。',
+    createdDate: 'June,2021',
+  },
+  {
+    image: 'images/YoutubeCloneApp.png',
+    title: 'Youtube Clone APP',
+    path: '',
+    description:
+      'YoutubeAPIを使用した、YoutubeのクローンAPPです。グローバルステイト管理の実践として開発しました。',
+    createdDate: 'May,2021',
+  },
+];
